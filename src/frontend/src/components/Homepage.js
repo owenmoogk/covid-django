941fa8402cf2dd1da624c8cdc898d7dc4c887a75
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2021-11-12 15:22:50 -0500

    added web scraping data to the app

--- src/frontend/src/components/Homepage.js
+++ src/frontend/src/components/Homepage.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Homepage(props){
+
+    const [data, setData] = useState()
+
+    useEffect(() => {
+        fetch('/api/data')
+        .then(response => response.json())
+        .then(json => setData(json))
+    }, [])
+
     return(
+        data?
         <div className='display'>
             <div className="item" style={{backgroundColor: "rgba(118, 200, 255, 0.9)"}}>
-                <div className="main">103 M</div>
+                <div className="main">{data.cases}</div>
                 <div className="sub">Cases</div>
             </div>
             <div className="item" style={{backgroundColor: "rgba(255, 118, 118, 0.9)"}}>
-                <div className="main">2.2 M</div>
+                <div className="main">{data.deaths}</div>
                 <div className="sub">Deaths</div>
             </div>
             <div className="item" style={{backgroundColor: "rgba(180, 255, 118, 0.9)"}}>
@@ -20,5 +30,6 @@ export default function Homepage(props){
                 <div className="sub">Points Today</div>
             </div>
         </div>
+        :null
     )
 }
